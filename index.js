d6426870b107797d1f0b4d85577dec637bd217b0
PFletcher <PFletcher>
2016-07-01 20:37:38 -0400

    set up post route

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 var express = require('express');
+var bodyParser = require('body-Parser');
+var lowdb = require('lowdb');
+var uuid = require('uuid');
 var server = express();
 
 var port = process.env.port || 8080;
+var db = lowdb('db.json');
 
+//database initialization
+db.defaults({todos: []})
+  .value(); //runs the previous sets of commands
 
-server.get('/todos/:id', function(request, response){
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
+
+server.get('/todos', function(request, response){
   response.send('GET todos');
-
 });
 
 server.get('/todos/:id', function(request, response){
   response.send('GET todos :id');
 })
 
-server.post('/todos/', function(request, response){
-  response.send('POST todos :id');
-})
+server.post('/todos', function(request, response){
+  var todo = {
+    id: uuid.v4(),
+    description: request.body.description,
+    isComplete: false
+  };
+
+  var result = db.get('todos')
+                 .push(todo)
+                 .last()
+                 .value();
+  response.send(result);
+});
 
 server.put('/todos/:id', function(request, response){
   response.send('PUT todos :id');
